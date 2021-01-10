baf03ba029ad226e551f6a3532686a7a20f19808
Lakshan Rasingolla <49824628+Rasi98@users.noreply.github.com>
2021-01-10 21:22:12 +0530

    create component step1

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import{BrowserRouter as Router,Rout} from 'react-router-dom';
+import{BrowserRouter as Router,Rout} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import ExecisesList from "./component/exercise-list.component";
+import Navbar from "./component/navbar.component";
+import EditExercise from "./component/edit-exercise.component";
+import CreateExercise from "./component/create-exercise.component";
+import CreateUser from "./component/create-user.component";
 
 function App() {
   return (
-    <div className="container">
-        Hello world!
-    </div>
+    <Router>
+        <Navbar/>
+        <br/>
+        <Rout path="/" exact component={ExecisesList}></Rout>
+        <Rout path="/edit/:id" component={EditExercise}></Rout>
+        <Rout path="create" component={CreateExercise}></Rout>
+        <Rout path="/user" component={CreateUser}></Rout>
+    </Router>
+    
   );
 }
 
